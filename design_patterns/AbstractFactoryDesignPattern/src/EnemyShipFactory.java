ab12f33fbea47f3e32241933b562253b59ca90d3
Caio Bianchi <caiohbs23@gmail.com>
2024-01-19 10:13:58 -0300

    finished "AbstractFactoryDesignPattern" project

---
+++ design_patterns/AbstractFactoryDesignPattern/src/EnemyShipFactory.java
@@ -0,0 +1,8 @@
+// With an Abstract Factory Pattern you won't just build ships, but also all the components for the ships.
+
+// Here is where you define the parts that are required if an object wants to be an enemy ship.
+public interface EnemyShipFactory {
+    public ESWeapon addESGun();
+
+    public ESEngine addESEngine();
+}

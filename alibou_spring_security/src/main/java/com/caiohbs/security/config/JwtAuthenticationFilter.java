da2398eb50a54615f48a362cc535e47c30828ac0
Caio Bianchi <caiohbs23@gmail.com>
2024-02-07 10:00:21 -0300

    added User and progress on JWT implementation

---
+++ alibou_spring_security/src/main/java/com/caiohbs/security/config/JwtAuthenticationFilter.java
@@ -0,0 +1,41 @@
+package com.caiohbs.security.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        final String jwtToken;
+        final String userEmail;
+        // Extracts the token from the header so that we can use it in other parts
+        // of the chain.
+        final String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwtToken = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwtToken);
+    }
+}
---
+++ alibou_spring_security/src/main/java/com/caiohbs/security/user/Role.java
@@ -0,0 +1,7 @@
+package com.caiohbs.security.user;
+
+public enum Role {
+
+    USER,
+    ADMIN
+}
